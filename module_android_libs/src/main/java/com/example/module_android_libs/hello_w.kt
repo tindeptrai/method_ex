6c4077ad8afa1f655cb28518dd30cd4c5b1669d9
nguyen Tin <nguyentrongtin1651999@gmail.com>
2021-05-28 12:04:38 +0700

    'abc'

---
+++ module_android_libs/src/main/java/com/example/module_android_libs/hello_w.kt
@@ -0,0 +1,26 @@
+package com.example.module_android_libs
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+
+class hello_w {
+    private lateinit var context : Context
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(context).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+                BatteryManager.EXTRA_SCALE, -1)
+        }
+        return batteryLevel
+    }
+
+}
